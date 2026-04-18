852447912cf74c3871fe4ddcd6fc4e850a935315
candlenimri <csalnimri22@cit.just.edu.jo>
2026-04-18 21:23:28 +0300

    .

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,13 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+=======
+>>>>>>> 35e81689b13453df6a4cd370b8f67d53e6d21b07
+>>>>>>> Stashed changes
 import uvicorn
 from main import app
 
