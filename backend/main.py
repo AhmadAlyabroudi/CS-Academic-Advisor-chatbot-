5d42ce242bf5946db32a69dc2abe7145514f3ecb
AhmadAlyabroudi <amalyabroudi22@cit.just.edu.jo>
2026-06-12 19:53:29 +0300

    seeding new student on verification process

--- backend/main.py
+++ backend/main.py
@@ -182,6 +182,7 @@ def seed():
             StudentVerification(email="amalnimri22@cit.just.edu.jo", university_id="166666"),
             StudentVerification(email="test22@cit.just.edu.jo", university_id="666666"),
             StudentVerification(email="test21@cit.just.edu.jo", university_id="666665"),
+
         ]
 
         for v in verification_data:
