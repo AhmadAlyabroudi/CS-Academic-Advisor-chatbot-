0dc700965b17360794c267b3a844b638120f3605
AhmadAlyabroudi <amalyabroudi22@cit.just.edu.jo>
2026-06-13 03:45:30 +0300

    seeding new verification data for student verification whitelist

--- backend/main.py
+++ backend/main.py
@@ -182,7 +182,7 @@ def seed():
             StudentVerification(email="amalnimri22@cit.just.edu.jo", university_id="166666"),
             StudentVerification(email="test22@cit.just.edu.jo", university_id="666666"),
             StudentVerification(email="test21@cit.just.edu.jo", university_id="666665"),
-
+            StudentVerification(email="test19@cit.just.edu.jo", university_id="666655"),
         ]
 
         for v in verification_data:
